67ac0b0989c05e449f7236e36ca1a02bfd6865e6
Alejandro Endo <theHilikus@gmail.com>
2022-03-31 23:43:19 -0400

    Don't use busy-wait
    Instead use a swing timer. The fact that it's a swing timer also reduces
    uncertainty between the time the ui change was requested and when it was
    actually done since now it's all happening in the same thread

--- src/main/java/com/github/neuralabc/spft/task/Trial.java
+++ src/main/java/com/github/neuralabc/spft/task/Trial.java
@@ -7,7 +7,11 @@ import com.github.neuralabc.spft.ui.ExperimentFrame;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.Map;
+import java.util.concurrent.CountDownLatch;
 
 /**
  * An actual task for subjects to perform
@@ -16,15 +20,50 @@ public class Trial {
     private static final Logger LOG = LoggerFactory.getLogger(Trial.class);
     private final TrialConfig config;
     private final SequenceConfig sequence;
+    private final CountDownLatch sync;
 
     public Trial(TrialConfig config, Map<String, SequenceConfig> sequencesPool) {
         this.config = config;
         this.sequence = sequencesPool.get(config.getSequenceRef());
+        sync = new CountDownLatch(1);
     }
 
     public void run(ExperimentFrame.Binding binding, OutputSection trialOutput) throws InterruptedException {
         LOG.info("\t\tStarting trial '{}'", config.getName());
-        for (int sequencePos = 0; sequencePos < sequence.getLength(); sequencePos++) {
+
+        int delay = 1000 / sequence.getFrequency();
+
+        Presentation presentation = new Presentation(trialOutput, binding);
+        Timer timer = new Timer(delay, presentation);
+        timer.setInitialDelay(0);
+        timer.setRepeats(true);
+        timer.start();
+
+        sync.await();
+        timer.stop();
+    }
+
+    public String getName() {
+        return config.getName();
+    }
+
+    private class Presentation implements ActionListener {
+        private final OutputSection trialOutput;
+        private final ExperimentFrame.Binding binding;
+        private int sequencePos;
+
+        public Presentation(OutputSection trialOutput, ExperimentFrame.Binding binding) {
+            this.trialOutput = trialOutput;
+            this.binding = binding;
+            sequencePos = 0;
+        }
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            if (sequencePos >= sequence.getLength()) {
+                //just in case timer is not cancelled before a next tick
+                return;
+            }
             if (sequence.getValuesLeft() != null) {
                 double leftReferenceValue = sequence.getValuesLeft().get(sequencePos);
                 trialOutput.addSample("leftReference", leftReferenceValue);
@@ -35,13 +74,10 @@ public class Trial {
                 trialOutput.addSample("rightReference", rightReferenceValue);
                 binding.setRightReferenceValue(rightReferenceValue);
             }
-
-            int delay = 1000 / sequence.getFrequency();
-            Thread.sleep(delay);
+            sequencePos++;
+            if (sequencePos >= sequence.getLength()) {
+                sync.countDown();
+            }
         }
-    }
-
-    public String getName() {
-        return config.getName();
     }
 }
